9562244cf0aae15e023a32fecd6fc59a73858392
xu1710 <a2275426161@163.com>
2018-11-08 22:32:23 +0800

    1.0.1

--- pages/index/calculator/cal.js
+++ pages/index/calculator/cal.js
@@ -1,10 +1,19 @@
 // pages/index/calculator/cal.js
 
-var ce = function(a, x, c) {
+var cal = function(a, x, c) {
   var data;
   a = parseFloat(a);
   c = parseFloat(c);
   switch (x) {
+    case "+":
+      data = a + c;
+      break;
+    case "-":
+      data = a - c;
+      break;
+    case "*":
+      data = a * c;
+      break;
     case "/":
       if (c == 0) {
         data = 0;
@@ -22,38 +31,103 @@ Page({
    */
   data: {
     temp: "0",
-    id1: "history",
     flag: true,
+    re: "0",
+    lastoper: "+",
+    id1: "history",
     id2: "clear",
-    id4: "/",
-    id5: "7",
-    id6: "8",
-    id7: "9",
+    id3: "back",
+    id4: "div",
+    id5: "num_7",
+    id6: "num_8",
+    id7: "num_9",
+    id8: "mul",
+    id9: "num_4",
+    id10: "num_5",
+    id11: "num_6",
+    id12: "sub",
+    id13: "num_1",
+    id14: "num_2",
+    id15: "num_3",
+    id16: "add",
+    id17: "negative",
+    id18: "num_0",
+    id19: "dot",
+    id20: "equ",
+    expr: "",
   },
 
   clickButton: function(e) {
     var data = this.data.re;
-    var tem = this.data.temp;
+    var tmp = this.data.temp;
     var lastoper1 = this.data.lastoper;
     var noNumFlag = this.data.flag;
     var expr1 = this.data.expr;
 
     if (e.target.id >= 'num_0' && e.target.id <= 'num_9') {
-      data += e.target.id.split('_')[1];
-
-      if (e.target.id == '0' || noNumFlag) {
+      data += e.target.id.split("_")[1];
+      if (this.data.re == '0' || noNumFlag) {
         data = e.target.id.split('_')[1];
       }
       noNumFlag = false;
-    }
-    else if(e.target.id=="back"){
-      if(data.toString().length>1){
-
+    } else if (e.target.id == "dot") {
+      if (data.toString().indexOf(".") == -1) {
+        data += ".";
+      } else if (e.target.id == "back") {
+        if (data.toString().length > 1) {
+          data += data.substr(o, data.toString().length - 1);
+        } else {
+          data = 0;
+        }
+      } else if (e.target.id == "div") {
+        expr1+=data.toString()+"/";
+          data =cal(tmp,lastoper1,data);
+          tmp=data;
+          lastoper1="/";
+      } else if (e.target.id == "mul") {
+        expr1 += data.toString() + "*";
+        data = cal(tmp, lastoper1, data);
+        tmp = data;
+        lastoper1 = "*";
+      } else if (e.target.id == "sub") {
+        expr1 += data.toString() + "-";
+        data = cal(tmp, lastoper1, data);
+        tmp = data;
+        lastoper1 = "-";
+      } else if (e.target.id == "add") {
+        expr1 += data.toString() + "+";
+        data = cal(tmp, lastoper1, data);
+        tmp = data;
+        lastoper1 = "+";
+      } else if (e.target.id == "equ") {
+        expr1 += data.toString() + "=";
+        data = cal(tmp, lastoper1, data);
+        tmp = data;
+        lastoper1 = "+";
+      } else{
+        noNumFlag=true;
+        console.log(e.target.id);
+        if (e.target.id=="clear"){
+          expr1="";
+          data=0;
+          tmp=0;
+          lastoper1="+";
+        } else if(e.target.id=="negative"){
+          data=-1*data;
+        }else if (e.target.id=="history"){
+          wx.navigateTo({
+            url: '../history/history',
+          })
+        }
       }
-    else if(0){
-
     }
-    }
+    this.setData({
+      re:data,
+      lastoper:lastoper1,
+      temp:tmp,
+      flag:noNumFlag,
+      expr:expr1
+    })
   },
 
 
@@ -61,7 +135,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+      
   },
 
   /**
